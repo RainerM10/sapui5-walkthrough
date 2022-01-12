640e72bcf3af8f57ae13a45cd724cebf680a5888
Rainer Menezes Vieira Silva <rainer.vieira.silva@avanade.com>
2022-01-12 17:49:58 -0300

    STEP 7: JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,25 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
 ], function(
 	Controller,
-    MessageToast
+	MessageToast,
+	JSONModel
 ) {
 	"use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+        onInit: function () {
+            // Set the data model on the view.
+            var oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
         onShowHello: function() {
             // Show a native or vanailla JS alert
             MessageToast.show("Hello There!");
