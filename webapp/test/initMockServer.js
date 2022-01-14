9d480fab468dce6826ae3225fc6808505bc35db9
Rainer Menezes Vieira Silva <rainer.vieira.silva@avanade.com>
2022-01-14 15:40:05 -0300

    STEP 27: Mock Server Configuration

---
+++ webapp/test/initMockServer.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "../localService/mockserver"
+], function(
+    mockserver
+) {
+	"use strict";
+    // Initialize the mock server
+    mockserver.init();
+    // Initialize the embedded componen on the HTML page
+    sap.ui.require(["sap/ui/core/ComponentSupport"]);
+});
